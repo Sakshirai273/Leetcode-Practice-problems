58518206945556c3deab448604854b9fb412e647
SAKSHI RAI <113307051+Sakshirai273@users.noreply.github.com>
2023-05-31 01:46:02 +0530

    Time: 114 ms (46.24%), Space: 54.3 MB (27.81%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.cpp
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -1,22 +1,51 @@
 class MyHashSet {
 public:
-    vector<bool> vec;
+    int numBuckets;
+    vector<list<int>> buckets;
+    int getHashValue(int key) {
+        return key%numBuckets;
+    }
+    
     MyHashSet() {
-        vec.resize(1000001 , false);
+        numBuckets = 15000;
+        buckets = vector<list<int>>(numBuckets, list<int>{});
     }
     
     void add(int key) {
-        vec[key] = true;
+        int index = getHashValue(key);
+        auto itr  = find(buckets[index].begin(), buckets[index].end(), key);
+        
+        if(itr == buckets[index].end())
+            buckets[index].push_back(key);
     }
     
     void remove(int key) {
-        vec[key] = false;
+        int index = getHashValue(key);
+        
+        auto itr  = find(buckets[index].begin(), buckets[index].end(), key);
+        
+        if(itr != buckets[index].end())
+            buckets[index].erase(itr);
     }
     
     bool contains(int key) {
-        return vec[key] == true;
+        int index = getHashValue(key);
+        
+        auto itr  = find(buckets[index].begin(), buckets[index].end(), key);
+        
+        return itr != buckets[index].end();
     }
 };
+
+
+//NOTE:
+/*
+You can use the information of load factor to resize the hashset if the Load Factor crosses 0.75
+int n = No. of elements inserted till now
+int b = Total Number of Buckets
+
+Load Factor = (n/b);
+*/
 
 /**
  * Your MyHashSet object will be instantiated and called as such:
