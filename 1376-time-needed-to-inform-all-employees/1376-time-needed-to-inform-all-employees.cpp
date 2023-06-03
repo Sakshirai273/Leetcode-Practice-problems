2a6dda724a048e9676fe2e9b4484173fefa0d30e
SAKSHI RAI <113307051+Sakshirai273@users.noreply.github.com>
2023-06-04 03:46:40 +0530

    Time: 560 ms (13.47%), Space: 162.5 MB (9.53%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,7 +1,22 @@
 class Solution {
 public:
     int maxTime= 0;
-    
+  void BFS(unordered_map<int , vector<int>> &adj , vector<int> & informTime , int headID ){
+      queue<pair<int, int>> q;
+      q.push({headID, 0});
+      
+      while(!q.empty()){
+          pair<int, int> temp = q.front();
+          q.pop();
+          int curr_employee = temp.first;
+          int curr_time = temp.second;
+          maxTime = max(maxTime , curr_time );
+          for(int &employee : adj[curr_employee]){
+              q.push({employee, curr_time + informTime[curr_employee]});
+              // time at which 'v' will receive the info
+          }
+      }
+  }
     void DFS(unordered_map<int , vector<int>> &adj , vector<int> & informTime , int curr_employee , int curr_time ){
         maxTime= max(maxTime, curr_time);
         for(int &employee : adj[curr_employee]){
@@ -17,7 +32,8 @@ public:
             if(manager_i != -1)
                 adj[manager_i].push_back(employee_i);
         }
-              DFS(adj , informTime , headID ,0 );
+              //DFS(adj , informTime , headID ,0 );
+        BFS(adj , informTime , headID );
             return maxTime;
         }
     };
