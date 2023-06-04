ec6c3481fce5c4973a991477821c167926998407
SAKSHI RAI <113307051+Sakshirai273@users.noreply.github.com>
2023-06-04 18:34:06 +0530

    Time: 24 ms (77.35%), Space: 13.7 MB (87.38%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    void dfs ( unordered_map<int,vector<int>> &adj, int u , vector<bool> &vis ){
+    int n;
+    void dfs(vector<vector<int>> &isConnected, int u, vector<bool>& vis) {
         vis[u] = true;
-        for(int &v : adj[u]){
-            if(!vis[v])
-            dfs(adj, v, vis);
+        
+        //Visit neighbours
+        for(int v = 0; v < n; v++) {
+            if(isConnected[u][v] == 1 && !vis[v]) {
+                dfs(isConnected, v, vis);
+            }
         }
     }
+    
+    
     int findCircleNum(vector<vector<int>>& isConnected) {
-      int n =  isConnected.size();  
-        //make graph
-        unordered_map<int,vector<int>>adj;
+        n = isConnected.size();
         
-        for(int i = 0 ; i<n;i++){
-            for(int j = 0 ; j<n ; j++){
-                if(isConnected[i][j] == 1){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
-                }
-            }
-        }
-           vector<bool> vis(n, false);
+        vector<bool> vis(n, false);
         int cnt = 0;
-        for(int i = 0 ; i <n ;i++){
-            if(!vis[i]){
+        
+        for(int i = 0; i<n; i++) {
+            if(!vis[i]) {
                 cnt++;
-                dfs(adj , i , vis);
+                dfs(isConnected, i, vis);
             }
         }
+        
         return cnt;
+        
     }
 };
