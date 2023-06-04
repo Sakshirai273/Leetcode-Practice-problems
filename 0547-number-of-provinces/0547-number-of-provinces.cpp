27ad3c03d0049f786d831237f59ecd523ef63957
SAKSHI RAI <113307051+Sakshirai273@users.noreply.github.com>
2023-06-04 18:48:44 +0530

    Time: 28 ms (51.98%), Space: 16 MB (7.18%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int n ;
-    void bfs (unordered_map<int,vector<int>> &adj, int u , vector<bool> &vis ){
+    void bfs (vector<vector<int>> &isConnected, int u , vector<bool> &vis ){
         queue<int>q;
         q.push(u);
         vis[u] = true;
@@ -9,30 +9,19 @@ public:
             int curr = q.front();
             q.pop();
             
-            for(int &v :adj[curr]){
-                if(!vis[v])
-                    bfs(adj,v,vis);
+            for(int v =0 ; v<n ;v++){
+                if(!vis[v] && isConnected[u][v] ==1)
+                    bfs(isConnected,v,vis);
             }
         }
 }
     int findCircleNum(vector<vector<int>>& isConnected) {
-      int n =  isConnected.size();  
-        //make graph
-        unordered_map<int,vector<int>>adj;
-        
-        for(int i = 0 ; i<n;i++){
-            for(int j = 0 ; j<n ; j++){
-                if(isConnected[i][j] == 1){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
-                }
-            }
-        }
+      n =  isConnected.size();  
            vector<bool> vis(n, false);
         int cnt = 0;
         for(int i = 0 ; i <n ;i++){
             if(!vis[i]){
-                   bfs(adj , i , vis);
+                   bfs(isConnected , i , vis);
                 cnt++;
              
             }
