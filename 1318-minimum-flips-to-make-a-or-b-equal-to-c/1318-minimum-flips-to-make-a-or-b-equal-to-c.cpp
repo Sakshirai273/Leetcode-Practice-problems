e33084aba4469e2469c9aac13fe81c8895d163c6
SAKSHI RAI <113307051+Sakshirai273@users.noreply.github.com>
2023-06-07 22:29:35 +0530

    Time: 4 ms (40.05%), Space: 5.9 MB (53.28%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -5,23 +5,22 @@ public:
         int bit2;
         int bit3;
         unordered_map<string , int> mp;
-        mp.insert({"0#0#1" , 1});
-        mp.insert({"1#0#0" , 1});
-         mp.insert({"0#1#0" , 1});
-         mp.insert({"1#1#0" , 2});
         int ans = 0;
-        while( a> 0 || c>0 || b>0){
+        while( a|| c|| b){
             bit1 = (a&1);
             bit2 = (b&1);
             bit3 = (c&1);
             
             if((bit1 | bit2) != bit3){
-                string str = to_string(bit1) + "#" + to_string(bit2) + "#" + to_string(bit3);
-                ans += mp[str];
+                if(bit1 & bit2)
+                    ans += 2;
+                else 
+                    ans ++;
+                
             }
             c = c>>1;
             b = b>>1;
-            a=  a>>1;
+            a = a>>1;
         }
         return ans;
         
