e95c47ceda91710f3e7ea3be5b4532164c369dc8
SAKSHI RAI <113307051+Sakshirai273@users.noreply.github.com>
2023-06-08 11:30:34 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (93.59%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     int minFlips(int a, int b, int c) {
-        int bit1;
-        int bit2;
-        int bit3;
-        unordered_map<string , int> mp;
-        int ans = 0;
-        while( a|| c|| b){
-            bit1 = (a&1);
-            bit2 = (b&1);
-            bit3 = (c&1);
-            
-            if((bit1 | bit2) != bit3){
-                if(bit1 & bit2)
-                    ans += 2;
-                else 
-                    ans ++;
-                
-            }
-            c = c>>1;
-            b = b>>1;
-            a = a>>1;
-        }
-        return ans;
+        int flips = 0;
         
+        while( a != 0 || b != 0 || c != 0){
+            if((c & 1) == 1){ // Right most bit (MSB) ==1
+                if((a&1) == 0 && (b&1) == 0){
+                    flips++;
+                    
+                }
+            }
+            else { // Right most bit is 0
+                if((a &1 ) == 1)
+                    flips++;
+                if((b&1) == 1)
+                    flips++;
+                //flips + = (a & 1) +(b & 1);
+            }
+            a >>= 1;
+            b >>= 1;
+            c>>= 1;
+        }
+        return flips;
     }
 };
