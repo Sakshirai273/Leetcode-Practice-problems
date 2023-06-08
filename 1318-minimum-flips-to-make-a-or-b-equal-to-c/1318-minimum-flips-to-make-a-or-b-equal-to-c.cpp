0515147d235afed661233673656db2f5647d4399
SAKSHI RAI <113307051+Sakshirai273@users.noreply.github.com>
2023-06-08 11:57:41 +0530

    Time: 3 ms (40.27%), Space: 5.9 MB (53.66%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -1,26 +1,13 @@
 class Solution {
 public:
     int minFlips(int a, int b, int c) {
-        int flips = 0;
+//         int result = (( a|b) ^ c); // no of flips 
         
-        while( a != 0 || b != 0 || c != 0){
-            if((c & 1) == 1){ // Right most bit (MSB) ==1
-                if((a&1) == 0 && (b&1) == 0){
-                    flips++;
-                    
-                }
-            }
-            else { // Right most bit is 0
-                if((a &1 ) == 1)
-                    flips++;
-                if((b&1) == 1)
-                    flips++;
-                //flips + = (a & 1) +(b & 1);
-            }
-            a >>= 1;
-            b >>= 1;
-            c>>= 1;
-        }
-        return flips;
+//         //if both bits are 1 in a and b - a&b
+//         int result1 = (a&b);
+//         int result2 = (result1 & result); 
+        
+//      //   return __builtin_popcount(result) + __builtin_popcount(result2);  // built in function to count no . of bitsets (no. of 1 bits)
+        return __builtin_popcount (((a|b) ^ c)) + __builtin_popcount((a&b) & (( a|b) ^ c)); 
     }
 };
