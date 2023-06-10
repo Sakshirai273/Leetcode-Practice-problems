b4c196e6a1cc11c856dce6792a3f930d46c51eda
SAKSHI RAI <113307051+Sakshirai273@users.noreply.github.com>
2023-06-11 01:09:21 +0530

    Time: 4 ms (42.79%), Space: 5.8 MB (92.04%) - LeetHub

--- 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
@@ -1,44 +1,39 @@
 class Solution {
 public:
+    typedef long long ll;
+    
+    ll getSumByFormula(ll count, ll x){
+        return (count*x) - (count*(count+1)/2);
+                            
+    }
     int maxValue(int n, int index, int maxSum) {
-        int lo=1;
-        int hi=maxSum;
-        int ans;
-        while(lo<=hi){
-            int mid=lo+(hi-lo)/2;
-            if(check(n,index,maxSum,mid)){
-                ans=mid;
-                lo=mid+1;
+     ll left = 1;
+        ll right = maxSum;
+         ll mid_val;
+        
+        int result  = 0;
+        while(left <= right){
+            mid_val = left + (right - left)/2;
+        ll left_count = min((ll)index, mid_val -1 );
+            ll left_sum = getSumByFormula (left_count ,mid_val );
+            left_sum +=  max((ll)0 , index -(mid_val - 1)); // extra ones if any
+                
+            ll right_count = min((ll)n - index-1 , mid_val -1 );
+            ll right_sum = getSumByFormula (right_count ,mid_val );
+            right_sum +=  max((ll)0 , n-index-1 -(mid_val - 1)); // extra ones if any
+            
+         ll total_sum = left_sum + mid_val +right_sum;
+            
+            if(total_sum <= maxSum){
+                result = max((ll)result , mid_val);
+                left = mid_val + 1;
+                
             }
-            else hi=mid-1;
+            else
+            {
+                right = mid_val-1;
+            }
         }
-        return ans;
-    }
-    bool check(int n,int idx,long long maxsum,int mid){
-        long long left=0;
-        long long right=0;
-        int leftrange=idx;
-        int rightrange=n-idx-1;
-                   //         left
-        if(mid-1>leftrange){
-            left=sum(mid-1)-sum(mid-1-leftrange);
-        }
-        else
-        {
-            left=sum(mid-1)+(leftrange-mid+1);
-        }
-                   //         right
-        if(mid-1>rightrange){
-            right=sum(mid-1)-sum(mid-1-rightrange);
-        }else{
-            right=sum(mid-1)+(rightrange-mid+1);
-        }
-        if(left+right+mid<=maxsum)
-            return true;
-        return false;
-    }
-    long long sum(long long n){
-        long long sum=(n*(n+1))/2;
-        return sum;
+        return result;
     }
 };
