c4b38d24387a3a58f82f3ee38c5375c8af768bb4
SAKSHI RAI <113307051+Sakshirai273@users.noreply.github.com>
2023-06-21 23:56:19 +0530

    Added solution - LeetHub

--- Reverse Coding - GFG/reverse-coding.cpp
+++ Reverse Coding - GFG/reverse-coding.cpp
@@ -6,7 +6,6 @@ using namespace std;
 class Solution {
   public:
     int sumOfNaturals(int n) {
-         // code here
         int mod=1e9+7;
         long ans=(long)n*(long)(n+1);
         ans/=2;
