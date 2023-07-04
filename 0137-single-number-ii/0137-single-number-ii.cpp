abda3a52e7481d8fee984066b4a41c1d503d1846
SAKSHI RAI <113307051+Sakshirai273@users.noreply.github.com>
2023-07-04 23:56:26 +0530

    Time: 6 ms (84.62%), Space: 9.6 MB (41.15%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,17 +1,50 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> m;
-        int ans=0;
-        for(int i =0;i<nums.size();i++){
-            m[nums[i]]++;
-        }
-        for(auto i:m){
-            if(i.second==1){
-                ans = i.first;
-                break;
+        int n=nums.size();
+        vector<int> ans(32,0);
+        for(int i=0;i<n;i++){
+            int temp=nums[i];
+            if(temp<0)
+                continue;
+            int cnt=31;
+            while(temp!=0){
+                int lastbit=temp&1;
+                ans[cnt]+=lastbit;
+                cnt--;
+                temp=temp>>1;
             }
         }
-        return ans;
+        long long finalans=0;
+        for(int i=31;i>=0;i--){
+            ans[i]=ans[i]%3;
+            if(ans[i]==1){
+                finalans+=pow(2,(31-i));
+            }
+        }
+        
+        if(finalans>0)
+        return finalans;
+        
+        for(int i=0;i<n;i++){
+            long long temp = nums[i];
+            if(temp>=0)
+                continue;
+            temp*=-1;
+            int cnt=31;
+            while(temp!=0){
+                int lastbit=temp&1;
+                ans[cnt]+=lastbit;
+                cnt--;
+                temp=temp>>1;
+            }
+        }
+        for(int i=31;i>=0;i--){
+            ans[i]=ans[i]%3;
+            if(ans[i]==1){
+                finalans+=pow(2,(31-i));
+            }
+        }
+        return finalans*-1;
     }
 };
