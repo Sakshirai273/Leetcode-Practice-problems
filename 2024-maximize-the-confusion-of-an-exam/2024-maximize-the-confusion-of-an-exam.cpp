106a9565d78c099dfd5b375aac182965a9f1dd9a
SAKSHI RAI <113307051+Sakshirai273@users.noreply.github.com>
2023-07-08 10:22:53 +0530

    Time: 91 ms (13.76%), Space: 10 MB (58.72%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -1,46 +1,21 @@
 class Solution {
 public:
     int maxConsecutiveAnswers(string answerKey, int k) {
-        
-        //case 1 : 'F' -> 'T'
-            int n= answerKey.length();
+        int n = answerKey.length();
         int result = 0 ;
+        int i=0 , j=0 ;
+        unordered_map<char, int> mp;
         
-        int i = 0 , j=0;
-        int countf = 0 ;
+        while(j<n){
+            mp[answerKey[j]]++;
         
-        while(j < n ){
-            if(answerKey[j] == 'F'){
-                countf++;
-            }
-            while(countf > k){
-                if(answerKey[i] == 'F'){
-                    countf--;
-                }
-                i++;
-            }
-            result = max(result , j-i+1);
-            j++;
+        while(min(mp['T'],mp['F']) > k){
+            mp[answerKey[i]]--;
+            i++;
         }
-        
-        //case 2 : 'T' -> 'F'
-             i = 0 , j=0;
-        int countt = 0 ;
-        
-        while(j < n ){
-            if(answerKey[j] == 'T'){
-                countt++;
-            }
-            while(countt > k){
-                if(answerKey[i] == 'T'){
-                    countt--;
-                }
-                i++;
-            }
-            result = max(result , j-i+1);
-            j++;
-        }
-        return result;
-        //TWO PASS SLIDING WINDOW 
+        result = max(result , j-i+1);
+        j++;
     }
+    return result;
+ }
 };
