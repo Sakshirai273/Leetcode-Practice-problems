e06656bf06a4fba9cafede5395467c5260764093
SAKSHI RAI <113307051+Sakshirai273@users.noreply.github.com>
2023-07-26 00:13:05 +0530

    Time: 12 ms (33.58%), Space: 9.4 MB (94.98%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,50 +1,17 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> ans(32,0);
-        for(int i=0;i<n;i++){
-            int temp=nums[i];
-            if(temp<0)
-                continue;
-            int cnt=31;
-            while(temp!=0){
-                int lastbit=temp&1;
-                ans[cnt]+=lastbit;
-                cnt--;
-                temp=temp>>1;
+        long mask = (1L<<31) , ans =0;
+        for( int i = 31; i >= 0 ; i--){
+            int cnt  = 0 ;
+            for( int j = 0 ; j<nums.size() ;j++){
+                if(nums[j] & mask)
+                    cnt++;
             }
-        }
-        long long finalans=0;
-        for(int i=31;i>=0;i--){
-            ans[i]=ans[i]%3;
-            if(ans[i]==1){
-                finalans+=pow(2,(31-i));
+            if( cnt %3 != 0)
+                ans+= mask;
+                mask >>= 1;
             }
-        }
-        
-        if(finalans>0)
-        return finalans;
-        
-        for(int i=0;i<n;i++){
-            long long temp = nums[i];
-            if(temp>=0)
-                continue;
-            temp*=-1;
-            int cnt=31;
-            while(temp!=0){
-                int lastbit=temp&1;
-                ans[cnt]+=lastbit;
-                cnt--;
-                temp=temp>>1;
-            }
-        }
-        for(int i=31;i>=0;i--){
-            ans[i]=ans[i]%3;
-            if(ans[i]==1){
-                finalans+=pow(2,(31-i));
-            }
-        }
-        return finalans*-1;
+            return ans;
     }
 };
