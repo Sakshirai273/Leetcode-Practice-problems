5d5d0bec136a6392faebb7e7fd3c1b13ee359153
SAKSHI RAI <113307051+Sakshirai273@users.noreply.github.com>
2023-08-16 19:20:19 +0530

    Time: 506 ms (52.69%), Space: 175.8 MB (45.55%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,103 +1,45 @@
 class LRUCache {
-//Doubly linked List creation
-  int cap;
-  struct Node{
-    int key;
-    int data;
-    struct Node*prev,*next;
-   Node(int x,int y)
-    {
-      key=x;
-      data =y;
-      prev=NULL;
-      next=NULL;
-    }
-  };
-  unordered_map<int,Node*> mp;
-     
-        
 public:
-          Node*head=new Node(0,0);
-        Node*tail=new Node(0,0);
-       
+    
+    list<int> dll; //key
+    map<int, pair<list<int>::iterator , int>> mp; //address  , int
+    int n;
+    
+    
     LRUCache(int capacity) {
-//assigning capcity and default doubly linked list
-        cap=capacity;
-        head->next=tail;
-        tail->prev=head;
+     n = capacity;
     }
-
-// function to create Node
-    Node* createNode(int key,int val)
-    {
-      Node*newNode=new Node(key,val);
-        newNode->next=head->next;
-            head->next->prev=newNode;
-            head->next=newNode;
-            newNode->prev=head;
-            return newNode;
-    }
+     void makeRecentlyUsed(int key ){
+         dll.erase(mp[key].first);
+         
+         dll.push_front(key);
+         
+         mp[key].first = dll.begin();
+     }
+    
     int get(int key) {
-      if(mp.find(key)!=mp.end())
-      {
-          auto it=mp.find(key);
-          int ans=it->second->data;
-          Node*newNode=createNode(it->second->key,it->second->data);
-          deleteNode(it->second);
-          mp.erase(key);
-          mp[key]=newNode;
-          return newNode->data;
-
-      }
-      return -1;
-    }
-
-//function to delete node
-    void deleteNode(Node* x)
-    {
-      Node*temp=x;
-      temp->prev->next=temp->next;
-      temp->next->prev=temp->prev;
-      temp->next=NULL;
-      temp->prev=NULL;
-      delete temp;
-      
-    }
+        if(mp.find(key) == mp.end()) // agar h hi nahi map me
+            return -1;
+        
+       makeRecentlyUsed(key);
+     return mp[key].second;
+}
     void put(int key, int value) {
-//checking if key present in map or not
-       if(mp.find(key)==mp.end())
-       {
-//checking if container has capacity or not
-         if(mp.size()<cap)
-         {
-
-            Node*newNode=createNode(key,value);
-            mp[key]=newNode;
-          }
-          else
-          {
-              Node*x=tail->prev;
-              int y=x->key;
-              deleteNode(x);
-              mp.erase(y);
-             Node*newNode=createNode(key,value);
-            mp[key]=newNode;
-          }
-       }
-       else
-       {
-         auto it=mp.find(key);
-         Node*x=it->second;
-              int y=x->key;
-              deleteNode(x);
-              mp.erase(y);
-               Node*newNode=new Node(key,value);
-            newNode->next=head->next;
-            head->next->prev=newNode;
-            head->next=newNode;
-            newNode->prev=head;
-            mp[key]=newNode;
-       } 
+        if(mp.find(key)  != mp.end()){
+            mp[key].second = value;
+                makeRecentlyUsed(key);
+        }
+        else{
+            dll.push_front(key);
+            mp[key] ={dll.begin(), value};
+            n--;
+        }
+        if(n < 0){
+            int key_to_be_del = dll.back();
+            mp.erase(key_to_be_del);
+            dll.pop_back();
+            n++;
+        }
     }
 };
 
