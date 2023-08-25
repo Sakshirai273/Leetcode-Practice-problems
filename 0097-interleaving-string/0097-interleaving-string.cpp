e0f5ee9c886c5dcaf7ebbf82dd7336afb6b6662f
SAKSHI RAI <113307051+Sakshirai273@users.noreply.github.com>
2023-08-25 21:43:54 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (85.24%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
     int m , n , N;
-    int t[101][101][201]; //{-1 , 0 , 1}
-    //o(m*n*N)
-    bool solve( int i , int j , int k , string& s1 , string &s2 , string &s3){
+    int t[101][101]; //{-1 , 0 , 1}
+    //o(m*n)
+    bool solve( int i , int j, string& s1 , string &s2 , string &s3){
         
-        if( i == m && j == n && k == N){
+        if( i == m && j == n && i+j== N){
             return true;
         }
-        if(k >= N ) //other string didn't get consumed at all
+        if(i+j >= N ) //other string didn't get consumed at all
             return false;
         
-        if(t[i][j][k] != -1)
-            return t[i][j][k];
+        if(t[i][j]!= -1)
+            return t[i][j];
         
         bool result = false;
         
-        if(s1[i] == s3[k]){
-            result = solve(i+1 , j , k+1 , s1 , s2 , s3);
+        if(s1[i] == s3[i+j]){
+            result = solve(i+1 , j , s1 , s2 , s3);
         }
         if(result == true)
-            return t[i][j][k] = result;
-        if(s2[j] == s3[k]){
-            result = solve( i , j+ 1, k+1 , s1, s2 , s3);
+            return t[i][j] = result;
+        
+        if(s2[j] == s3[i+j]){
+            result = solve( i , j+1, s1, s2 , s3);
         }
-        return t[i][j][k] = result;
+        return t[i][j] = result;
     }
     bool isInterleave(string s1, string s2, string s3) {
         m = s1.length();
         n = s2.length();
         N = s3.length();
         memset( t , -1 , sizeof(t));
-        return solve( 0 , 0, 0 , s1, s2 ,s3);
+        return solve( 0 , 0 , s1, s2 ,s3);
     }
 };
