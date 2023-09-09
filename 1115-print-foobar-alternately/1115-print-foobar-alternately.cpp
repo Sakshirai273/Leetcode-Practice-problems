e066158980b97b533ed72a8ecca97363536dad17
SAKSHI RAI <113307051+Sakshirai273@users.noreply.github.com>
2023-09-09 20:47:06 +0530

    Time: 8 ms (74.34%), Space: 8.3 MB (26.03%) - LeetHub

---
+++ 1115-print-foobar-alternately/1115-print-foobar-alternately.cpp
@@ -0,0 +1,43 @@
+class FooBar {
+private:
+    int n;
+    mutex m;
+    condition_variable cv;
+    bool turn;
+    
+
+public:
+    FooBar(int n) {
+        this->n = n;
+        this->turn=0;
+    }
+
+    void foo(function<void()> printFoo) {
+        
+        for (int i = 0; i < n; i++) {
+            unique_lock<mutex> lock (m);
+            while(turn==1)
+                cv.wait(lock);
+            
+            
+        	printFoo();
+            turn =1;
+            cv.notify_all();
+        }
+    }
+
+    void bar(function<void()> printBar) {
+        
+        for (int i = 0; i < n; i++) {
+             unique_lock<mutex> lock (m);
+            while(turn==0)
+                cv.wait(lock);
+            
+            
+        	printBar();
+            turn =0;
+            cv.notify_all();
+        }
+    }
+};
+
