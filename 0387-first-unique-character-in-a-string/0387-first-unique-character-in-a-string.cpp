8083a766ef64c54a5632b25c090266c31e7d3289
SAKSHI RAI <113307051+Sakshirai273@users.noreply.github.com>
2023-09-12 02:50:37 +0530

    Time: 33 ms (59.40%), Space: 10.7 MB (50.19%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -7,7 +7,7 @@ public:
             mp[s[i]]++;
         }
          for(int i =  0 ; i< s.length() ;i++){
-            if(mp[s[i]]  < 2)
+            if(mp[s[i]] == 1)
                 return i;
         }
             return -1;
