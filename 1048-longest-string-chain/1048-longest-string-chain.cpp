1ae4a2bdf2b60ecc0f1e079798b0103ea07827f8
SAKSHI RAI <113307051+Sakshirai273@users.noreply.github.com>
2023-09-23 23:43:52 +0530

    Time: 2162 ms (5.03%), Space: 645.1 MB (8.09%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
+    static bool cmp(string a, string b){
+        return a.length()<b.length();
+    }
+    bool isSubsequence(string s, string t) {
+        int j=0;
+        for(int i=0; i<t.length();i++){
+            if(t[i]==s[j]){
+                j++;
+            }
+        }
+        if(s.length()==j)return true;
+        return false;
+    }
 
     int longestStrChain(vector<string>& words) {
-        // Sort the words by their lengths
-        sort(words.begin(), words.end(), [](const string& a, const string& b) {
-            return a.length() < b.length();
-        });
-
-        // Map to store the longest chain length for each word
-        unordered_map<string, int> longestChainLength;
-
-        // Initialize the answer
-        int maxChainLength = -1;
-
-        for(auto &word: words){
-            // Initialize the chain length for the current word
-            longestChainLength[word] = 1;
-
-            // Try removing one character at a time from the word and check if the resulting word exists
-            for(int i = 0 ; i < word.size() ; i++){
-                string reducedWord = word.substr(0, i) + word.substr(i + 1) ;
-
-                // If the reduced word exists in the map
-                if(longestChainLength.find(reducedWord) != longestChainLength.end())
-                    // Update the chain length for the current word
-                    longestChainLength[word] = max(longestChainLength[word], longestChainLength[reducedWord] + 1) ;
+        sort(words.begin(),words.end(),cmp);
+        int n=words.size();
+        vector<int> dp(n, 1);
+        int maxLen = 1;
+        
+        for(int i=1; i<n; i++){
+            for(int j=0; j<i; j++){
+                if(isSubsequence(words[j], words[i]) && words[j].length()+1==words[i].length()){
+                    dp[i] = max(dp[i], dp[j]+1);
+                }
             }
-
-            // Update the maximum chain length seen so far
-            maxChainLength = max(maxChainLength, longestChainLength[word]) ;
+            maxLen = max(maxLen, dp[i]);
         }
-
-        return maxChainLength;
+        return maxLen;
     }
 };
