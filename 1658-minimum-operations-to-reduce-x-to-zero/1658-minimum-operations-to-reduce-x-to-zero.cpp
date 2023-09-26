7315740efb29ba07f68ce72162063ce1169f6337
SAKSHI RAI <113307051+Sakshirai273@users.noreply.github.com>
2023-09-27 00:40:38 +0530

    Time: 119 ms (80.25%), Space: 98.7 MB (83.57%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,13 +2,18 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
         int totalSum = 0;
+        
         for (int num : nums)
             totalSum += num;
+        
         int maxLength = -1, currSum = 0;
+        
         for (int l = 0, r = 0; r < nums.size(); r++) {
             currSum += nums[r];
+            
             while (l <= r && currSum > totalSum - x)
                 currSum -= nums[l++];
+            
             if (currSum == totalSum - x)
                 maxLength = max(maxLength, r - l + 1);
         }
