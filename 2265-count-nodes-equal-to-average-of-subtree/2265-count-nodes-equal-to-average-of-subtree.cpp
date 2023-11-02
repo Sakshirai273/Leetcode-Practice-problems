1dc17585a8da1ed435421730ad38908d5d2b6ae0
SAKSHI RAI <113307051+Sakshirai273@users.noreply.github.com>
2023-11-03 02:43:26 +0530

    Time: 4 ms (80.86%), Space: 12.4 MB (34.25%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -11,35 +11,27 @@
  */
 class Solution {
 public:
-  int count = 0;  // Initialize a variable to count valid subtrees.
+  int count = 0;  
 
-  // A helper function to calculate the sum and count of valid subtrees.
   pair<int, int> solve(TreeNode* root) {
-      
-      // If the node is null, return 0 for both sum and count.
     if (root == nullptr)
         return {0, 0};  
-    int subtreeSum = root->val;  // Initialize the sum with the current node's value.
-    int subtreeNodeCount = 1;    // Initialize the node count with 1 for the current node.
-
-    // Recursively calculate the sum and node count for the left and right subtrees.
+    int subtreeSum = root->val;  
+    int subtreeNodeCount = 1;    
+    
     pair<int, int> leftSubtree = solve(root->left);
     pair<int, int> rightSubtree = solve(root->right);
-
-    // Update the sum and node count with values from left and right subtrees.
     subtreeSum += leftSubtree.first + rightSubtree.first;
     subtreeNodeCount += leftSubtree.second + rightSubtree.second;
-
-    // Check if the current node satisfies the condition (value is equal to the average).
+      
     if (root->val == (subtreeSum / subtreeNodeCount)) {
-      count++;  // Increment the count if the condition is met.
+      count++;  
     }
-
     return {subtreeSum, subtreeNodeCount};
   }
 
   int averageOfSubtree(TreeNode* root) {
-    pair<int, int> result = solve(root);  // Calculate the sum and count.
-    return count;  // Return the count of valid subtrees.
+    pair<int, int> result = solve(root); 
+    return count;  
   }
 };
